db366109ffed11cc2dfd464cb8c51c2c96a93683
Eliseo Martelli <me@eliseomartelli.it>
2024-10-07 23:23:10 +0200

    feat: return config struct from env

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -13,6 +14,35 @@ type Config struct {
 	DefaultTTL      time.Duration
 	CleanupInterval time.Duration
 }
+
+// Load reads environment variables and returns a Config struct
+func Load() *Config {
+	lockKeysEnv := getEnv("LOCK_KEYS", "")
+	var lockKeys []string
+	if lockKeysEnv != "" {
+		lockKeys = strings.Split(lockKeysEnv, ",")
+		for i := range lockKeys {
+			lockKeys[i] = strings.TrimSpace(lockKeys[i])
+		}
+	} else {
+		log.Println("No LOCK_KEYS defined. All keys will be allowed.")
+	}
+	defaultTTL := valueAsDuration(
+		getEnv("DEFAULT_TTL", ""),
+		60*time.Second)
+	cleanupInterval := valueAsDuration(
+		getEnv("CLEANUP_INTERVAL", ""),
+		30*time.Second)
+
+	return &Config{
+		DBPath:          getEnv("DB_PATH", "locks.db"),
+		Port:            getEnv("PORT", "8080"),
+		LockKeys:        lockKeys,
+		DefaultTTL:      defaultTTL,
+		CleanupInterval: cleanupInterval,
+	}
+}
+
 func getEnv(key, defaultValue string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
